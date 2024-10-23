314c9b1bc197d1517afd5cb0e97b3232f9a5b715
7herion <1037762715.mz@gmail.com>
2024-10-23 19:32:39 +0200

    :wrench: Update settings

--- depth_anything.py
+++ depth_anything.py
@@ -6,12 +6,11 @@ from depth_anything_v2.dpt import DepthAnythingV2
 def depth_calculation(filename: str):
     '''Calcola la distanza dei punti nell'immagine'''
 
-    # Leggo alcune cose da settings.yaml
-    with open('settings.yaml', 'r') as settings_file:
+    with open('config/settings.yaml', 'r') as settings_file:
         env_settings = yaml.safe_load(settings_file)
     encoder_config      = env_settings['encoder']       # 'vitb'
     input_size_config   = env_settings['input_size']    # 518
-    max_depth_config    = env_settings['max_depth']     # 20
+    max_depth_config    = env_settings['max_depth']     # 80
     model_path_config   = env_settings['model_path']    # checkpoints/depth_anything_v2_metric_vkitti_vitb.pth
 
     model_configs = {
@@ -25,16 +24,8 @@ def depth_calculation(filename: str):
     depth_anything = DepthAnythingV2(**{**model_configs[encoder_config], 'max_depth': max_depth_config})
     depth_anything.load_state_dict(torch.load(model_path_config, map_location='cpu'))
     depth_anything = depth_anything.to(DEVICE).eval()
-    
+
     raw_image = cv2.imread(filename)
     depth_map = depth_anything.infer_image(raw_image, input_size_config)
 
-    # Ritorna una cosa simile a questa
-    # [[4.7106595 4.7855444 4.860429  ... 4.098736  4.00116   3.9036021]
-    #  [4.793133  4.885724  4.978315  ... 4.109568  4.055221  4.000884 ]
-    #  [4.8756065 4.9859037 5.0962014 ... 4.1204004 4.1092825 4.0981655]
-    #  ...
-    #  [8.532101  8.525809  8.519517  ... 8.92571   8.923376  8.921043 ]
-    #  [8.551974  8.5424    8.532825  ... 8.934305  8.93417   8.934035 ]
-    #  [8.571844  8.558989  8.546132  ... 8.942899  8.944961  8.947023 ]]
     return depth_map
