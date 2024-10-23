ca9f96aacace45f11e8126898a9548d401b91ca3
7herion <1037762715.mz@gmail.com>
2024-10-23 19:33:34 +0200

    :recycle: Uniform payload structure

--- app.py
+++ app.py
@@ -2,47 +2,30 @@ from flask import jsonify, Flask, request
 import utils
 from depth_anything import depth_calculation
 import numpy as np
+import ast
 
 app = Flask(__name__)
 
 @app.route("/distance-imagelist", methods=["POST"])
 def distance_image_list():
-    '''
-    Endpoint per stimare la distanza di una lista di immagini
-
-    Esempio di payload per la request:
-    [
-        {
-            "image_path": "test/img__136.jpg",
-            "bounding_box": [
-                0.5697,
-                0.3929,
-                0.1525,
-                0.2355
-            ]
-        }
-    ]
-    '''
+    '''Endpoint per stimare la distanza di una lista di immagini'''
 
     image_list = request.json
-    result_list: list = []
 
     for item in image_list:
-        item_bbox = item['bounding_box']
-        item_path = item['image_path']
+        item_path = item['filename']
 
         depth_map = depth_calculation(item_path)
 
-        b_box_depth_map = utils.getDepthMapBBoxArea(depth_map, item_bbox)
-        distance_estimate = round(np.percentile(b_box_depth_map, 20), 2) # 20esimo percentile arrotondato a 2 decimali
+        for bounding_box in item['bb_box']:
+            bbox_coordinates = ast.literal_eval(bounding_box['box'])
 
-        result_list.append({
-            'bounding_box': item_bbox,
-            'image_path': item_path,
-            'distance': distance_estimate,
-        })
+            b_box_depth_map = utils.getDepthMapBBoxArea(depth_map, bbox_coordinates)
+            distance_estimate = round(np.percentile(b_box_depth_map, 20), 2) # 20esimo percentile arrotondato a 2 decimali
 
-    return jsonify(result_list)
+            bounding_box['distance'] = distance_estimate
+
+    return jsonify(image_list)
 
 
 if __name__ == '__main__':
