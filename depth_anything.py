a06167cbb35a7118e13d4f022f905f0ce3088c9d
7herion <1037762715.mz@gmail.com>
2024-11-18 19:32:56 +0100

    :zap: Add cache

--- depth_anything.py
+++ depth_anything.py
@@ -2,17 +2,11 @@ import cv2
 import torch
 import yaml
 from depth_anything_v2.dpt import DepthAnythingV2
+from functools import lru_cache
 
-def depth_calculation(filename: str):
-    '''Calcola la distanza dei punti nell'immagine'''
 
-    with open('config/settings.yaml', 'r') as settings_file:
-        env_settings = yaml.safe_load(settings_file)
-    encoder_config      = env_settings['encoder']       # 'vitb'
-    input_size_config   = env_settings['input_size']    # 518
-    max_depth_config    = env_settings['max_depth']     # 80
-    model_path_config   = env_settings['model_path']    # checkpoints/depth_anything_v2_metric_vkitti_vitb.pth
-
+@lru_cache(maxsize=3)
+def load_model(encoder_config, model_path_config, max_depth_config):
     model_configs = {
         'vits': {'encoder': 'vits', 'features': 64, 'out_channels': [48, 96, 192, 384]},
         'vitb': {'encoder': 'vitb', 'features': 128, 'out_channels': [96, 192, 384, 768]},
@@ -24,6 +18,21 @@ def depth_calculation(filename: str):
     depth_anything = DepthAnythingV2(**{**model_configs[encoder_config], 'max_depth': max_depth_config})
     depth_anything.load_state_dict(torch.load(model_path_config, map_location='cpu'))
     depth_anything = depth_anything.to(DEVICE).eval()
+
+    return depth_anything
+
+
+def depth_calculation(filename: str):
+    '''Calcola la distanza dei punti nell'immagine'''
+
+    with open('config/settings.yaml', 'r') as settings_file:
+        env_settings = yaml.safe_load(settings_file)
+    encoder_config      = env_settings['encoder']       # 'vitb'
+    input_size_config   = env_settings['input_size']    # 518
+    max_depth_config    = env_settings['max_depth']     # 80
+    model_path_config   = env_settings['model_path']    # checkpoints/depth_anything_v2_metric_vkitti_vitb.pth
+
+    depth_anything = load_model(encoder_config, model_path_config, max_depth_config)
 
     raw_image = cv2.imread(filename)
     depth_map = depth_anything.infer_image(raw_image, input_size_config)
